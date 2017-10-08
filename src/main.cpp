0747c5c0f69c87cc011202d2c5277e485c7abf4e
Leonidas Pentovelis <lppento@live.fr>
2017-10-08 22:32:22 +0200

    Custom gui layers

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
 #include "include/rltk.hpp"
 #include <sstream>
 
-using namespace rltk;
-using namespace rltk::colors;
+enum gui_layer {
+	MAIN_LAYER,
+};
+
+void resize_main(rltk::layer_t *l, int w, int h) {
+	l->w = w;
+	l->h = h;
+}
 
 void tick(double duration_ms) {
 	std::stringstream ss;
 	ss << "Frame duration: " << duration_ms << " ms (" << (1000.0 / duration_ms) << " FPS).";
-	console->clear();
-	console->print(1, 1, "Hello World", WHITE, BLACK);
-	console->print(1, 2, ss.str(), YELLOW, BLUE);
+	rltk::term(MAIN_LAYER)->clear();
+	rltk::term(MAIN_LAYER)->print(1, 1, "Hello World", rltk::colors::WHITE, rltk::colors::BLACK);
+	rltk::term(MAIN_LAYER)->print(1, 2, ss.str(), rltk::colors::YELLOW, rltk::colors::BLUE);
 }
 
 int main()
 {
-	init(config_simple("../assets", 80, 50));
-	run(tick);
+	rltk::init(rltk::config_advanced("../assets", 720, 480));
+	rltk::gui->add_layer(MAIN_LAYER, 0, 0, 720, 480, "8x8", resize_main);
+	rltk::run(tick);
 	return 0;
 }
