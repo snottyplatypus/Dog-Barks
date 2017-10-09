50db78d6185899cd8126985b8ae907f14f818442
Leonidas Pentovelis <lppento@live.fr>
2017-10-09 16:22:49 +0200

    inline gui functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include "include/rltk.hpp"
-#include <sstream>
 #include "utils/gui.hpp"
 #include "systems/ActorSystem.hpp"
 
 ActorSystem player;
 
-void resize_main(rltk::layer_t *l, int w, int h) {
-	l->w = w;
-	l->h = h;
-}
-
 void tick(double duration_ms) {
-	std::stringstream ss;
-	//ss << "Frame duration: " << duration_ms << " ms (" << (1000.0 / duration_ms) << " FPS).";
 	rltk::term(MAIN_LAYER)->clear();
 	player.update();
-	//rltk::term(MAIN_LAYER)->print(1, 2, ss.str(), rltk::colors::YELLOW, rltk::colors::BLUE);
 }
 
 int main()
--- src/utils/gui.hpp
+++ src/utils/gui.hpp
@@ -3,3 +3,7 @@ enum gui_layer {
 	MAIN_LAYER,
 };
 
+inline void resize_main(rltk::layer_t *l, int w, int h) {
+	l->w = w;
+	l->h = h;
+}
