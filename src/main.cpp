a0b7b29b27882cfef13bcd8e6e7f72b5242448b1
Leonidas Pentovelis <lppento@live.fr>
2017-10-14 20:13:04 +0200

    Virtual terminals

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "include/rltk/rltk.hpp"
 #include "utils/gui.hpp"
 #include "systems/PlayerSystem.hpp"
@@ -7,13 +8,19 @@ Level level;
 
 void tick(double duration_ms) {
 	rltk::term(MAIN_LAYER)->clear();
+	rltk::term(GUI_LAYER)->clear();
+	rltk::term(MISC_LAYER)->clear();
 	level.update();
 }
 
 int main()
 {
 	rltk::init(rltk::config_advanced("../assets", 720, 480));
-	rltk::gui->add_layer(MAIN_LAYER, 0, 0, 720, 480, "8x8", resize_main);
+	int windowWidth = 720;
+	int windowHeight = 480;
+	rltk::gui->add_layer(MAIN_LAYER, 0, 0, windowWidth, windowHeight, "8x8", resize_main);
+	rltk::gui->add_layer(GUI_LAYER, 0, 0, windowWidth, windowHeight, "8x8", resize_main);
+	rltk::gui->add_layer(MISC_LAYER, 0, 0, windowWidth, windowHeight, "8x8", resize_main);
 	rltk::get_window()->setFramerateLimit(60);
 	rltk::run(tick);
 	return 0;
--- src/utils/gui.hpp
+++ src/utils/gui.hpp
@@ -1,6 +1,8 @@
 #pragma once
 enum gui_layer {
 	MAIN_LAYER,
+	GUI_LAYER,
+	MISC_LAYER,
 };
 
 inline void resize_main(rltk::layer_t *l, int w, int h) {
